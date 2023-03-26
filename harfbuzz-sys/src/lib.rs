491f5a4c3cdefa2eeaad7fdbf2ace9d8679df8e8
Francesca Lovebloom <franlovebloom@gmail.com>
2023-03-25 18:47:01 -0700

    hb_ft bindings + remove everything I don't need

--- harfbuzz-sys/src/lib.rs
+++ harfbuzz-sys/src/lib.rs
@@ -1,21 +1,9 @@
-#[cfg(any(target_os = "android", all(unix, not(target_vendor = "apple")), feature="build-native-freetype"))]
-extern crate freetype;
-
-#[cfg(target_vendor = "apple")]
-pub mod coretext;
-
-#[cfg(target_family = "windows")]
-pub mod directwrite;
-
-#[cfg(any(target_os = "android", all(unix, not(target_vendor = "apple")), feature="build-native-freetype"))]
-extern "C" {
-    pub fn hb_ft_font_create_referenced(face: freetype::freetype::FT_Face) -> *mut hb_font_t;
-}
-
-#[allow(non_camel_case_types)]
-#[allow(non_snake_case)]
-#[allow(non_upper_case_globals)]
-#[allow(clippy::unreadable_literal)]
+#[allow(
+    non_camel_case_types,
+    non_snake_case,
+    non_upper_case_globals,
+    clippy::unreadable_literal
+)]
 mod bindings;
 
 pub use bindings::*;
--- harfbuzz-sys/wrapper.h
+++ harfbuzz-sys/wrapper.h
@@ -1,3 +1,4 @@
 #include "harfbuzz/src/hb.h"
 #include "harfbuzz/src/hb-ot.h"
 #include "harfbuzz/src/hb-aat.h"
+#include "harfbuzz/src/hb-ft.h"
